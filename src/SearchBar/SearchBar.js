7808dad57368dad866b3f5623056b1c12cd0019e
gkken <kokken95@gmail.com>
2022-07-12 10:06:58 +0800

    search bar v1

--- src/SearchBar/SearchBar.js
+++ src/SearchBar/SearchBar.js
@@ -1,7 +1,41 @@
 import './SearchBar.css'
+import {useState} from 'react'
 
 function SearchBar() {
-  return <div></div>
+  let [searchBar1Content, setSearchBar1Content] = useState([])
+  let [searchBarList, setSearchBarList] = useState([1])
+
+  function handleSubmit(e){
+    e.preventDefault()
+    const formData = new FormData(e.target)
+    const formEntries = Object.fromEntries(formData)
+    for (let key in formEntries){
+      console.log(`the value of ${key} is ${formEntries[key]}`)
+    }
+    // console.log(formEntries.choice0)
+  }
+
+  function addParameter(){
+    setSearchBarList([...searchBarList, 1])
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit} className="searchForm">
+      {searchBarList.map((_, idx) => <div className={`searchBar${idx}`}>
+        <input type="text" name={`searchBarText${idx}`}/>
+         <select name={`choice${idx}`}>
+           <option value="artist">Artist</option>
+           <option value="track">Track</option>
+           <option value="genre">Genre</option>
+         </select>
+       </div>)}
+        <button>Submit</button>
+      </form>
+      <button onClick={addParameter}>Add Parameter</button>
+      
+    </div>
+  )
 }
 
 export default SearchBar
