d08660c8a01b4dc449841973c6af6c95b02a352d
Mun Hin <obh555@gmail.com>
2022-07-13 17:18:51 +0800

    delete button

--- src/Playlist/Track.js
+++ src/Playlist/Track.js
@@ -1,22 +1,26 @@
-import Stack from '@mui/material/Stack';
-import RemoveCircleOutlineIcon from '@mui/icons-material/RemoveCircleOutline';
-import PlayArrowIcon from '@mui/icons-material/PlayArrow';
-import './Track.css'
+import Stack from "@mui/material/Stack";
+import RemoveCircleOutlineIcon from "@mui/icons-material/RemoveCircleOutline";
+import PlayArrowIcon from "@mui/icons-material/PlayArrow";
+import "./Track.css";
 
 function Track(props) {
-
   const { track, idx, removeTrack, handleTracks } = props.data;
-
   const runtime = (ms) => {
     const minutes = Math.floor(ms / 60000);
     const seconds = ((ms % 60000) / 1000).toFixed(0);
-
-    return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
-  }
+    return minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
+  };
 
   return (
     <Stack mt={2}>
-      <div className="track-container" onClick={() => handleTracks(track)}>
+      <div
+        className="track-container"
+        onClick={(event) => {
+          console.log(event.target.animVal);
+          if (event.target.className === "block") return;
+          handleTracks(track);
+        }}
+      >
         <div className="num-play-wrapper">
           <h4 className="num">{idx + 1}</h4>
           <div className="play-btn">
@@ -31,11 +35,12 @@ function Track(props) {
         <p>{track.album.name}</p>
         <p>{runtime(track.duration_ms)}</p>
         <div className="remove-btn">
-          <RemoveCircleOutlineIcon onClick={() => removeTrack(idx)} style={{ margin: `auto 0` }} />
+          <div className="block" onClick={() => removeTrack(idx)}></div>
+          <RemoveCircleOutlineIcon sx={{ margin: `auto 0` }} />
         </div>
       </div>
     </Stack>
   );
 }
 
-export default Track
+export default Track;
